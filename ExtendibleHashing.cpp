0ec8c3fbe4ac198579ef71524ec661b8ab95dd8d
Durgesh Kumar <k.durgesh@iitg.ernet.in>
2017-02-03 15:50:20 +0530

    Added main function, print function and call to Bucket class

--- ExtendibleHashing.cpp
+++ ExtendibleHashing.cpp
@@ -22,7 +22,7 @@ class Bucket
         {  int local_depth =0;
            int max_size = size 
            int cur_size =-1; // -1 indicates Empty Bucket 
-           int head[max_size]; // A dynamic Array 
+           int A[max_size]; // A dynamic Array 
         }
         ~LinkedList()
         {
@@ -38,16 +38,73 @@ class Bucket
                 }
         }
         void remove()
-        {
+        { 
 
         }
         void removeAll()
         {
         }
         void print()
-        {
-
-        }
+        {	
+		int i;
+		cout << "-----------------------------------------" << endl;
+		cout << "--------Printing Bucket----" << endl;
+		for(i=0;i<cur_size; i++)
+			cout << A[i] << '\t';
+		cout << "--------End of Bucket---" << endl;     
+	}
 
  }
+
+
+int printMenu() {
+	system("clear");
+	cout << "-----------------------------------------" << endl;
+	cout << "Extendible Hashing Implementation." << endl;
+	cout << "-----------------------------------------" << endl;
+	cout << "\t\t 1. Insert a Record" << endl;
+	cout << "\t\t 2. Search a Record" << endl;
+	cout << "\t\t 3. Delete a Record" << endl;
+	cout << "\t\t 4. Show All Records"<< endl;
+	cout << "\t\t 5. Delete All Records" << endl;
+	cout << "\t\t 6. Exit" << endl;
+	int choice;
+	cout << "\nChoice : ";
+	cin >> choice;
+	cout.flush();
+	return choice;
+}
+
+int main()
+{
+	int n,value;
+	char ch;
+	cout << "Enter bucket-size : ";
+	cin >> n;
+	Bucket *B = new Bucket(n);
+	while(true) {
+		int choice = printMenu();
+		ch = getchar();  // eat newline
+		switch(choice) {
+			case 1:   // Insert a record
+				{
+					
+					cout << "Enter value to be added : ";
+					cin >> value;
+					B->insert(value);
+					break;
+				}
+			
+			case 2:   // show All Records
+			        {
+					system("clear");
+					B->print();
+					ch = getchar();
+					break;
+				}
+		}
+	}
+
+	return 0;
+}
 
