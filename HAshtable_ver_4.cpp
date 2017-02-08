2a2e5ee2282de19ac9474182bee4e444ef502099
Durgesh Kumar <k.durgesh@iitg.ernet.in>
2017-02-09 02:09:01 +0530

    Insertion Completed :

--- HAshtable_ver_4.cpp
+++ HAshtable_ver_4.cpp
@@ -72,6 +72,19 @@ bool isFull(Bucket *b, int overflow_option)
 	}
 }
 
+void emptyBucket(Bucket *b)
+{
+
+	vector<int> node=b->node;
+	vector<int> overflownode = b->overflow_page;
+	node.erase(node.begin(),node.end());
+	//emptying overflow_page
+	overflownode.erase(overflownode.begin(),overflownode.end());
+	b->node=node;
+	b->overflow_page=overflownode; 
+	//b->cur_size = -1;
+}
+
 void print(HashTable &h)
 {
 	vector<Bucket*> directory=h.directory;
@@ -98,7 +111,8 @@ void print(HashTable &h)
 		cout <<  i << "     \t| ";		
 		if (i>=dir_half_size && h.global_depth != b->local_depth) // Escape printing for Mirror Image
 		{
-			cout <<" \t|  Mirror Image |" << "  Mirror Image" << endl;
+			//cout << " GLOBAL Depth : " << h.global_depth << " Local DEPTH : " <<   b->local_depth << endl;
+			cout <<" Mirror Image |" << "  Mirror Image" << endl;
 			continue;
 		}
 		else if(node_size ==0)
@@ -106,6 +120,7 @@ void print(HashTable &h)
 			cout <<"   Empty   |" << " No Overflow Page" << endl;
 			continue;
 		}
+		//cout << " \nLine 122 : "<<endl ;
 		for(int j=0;j<node.size();j++)
 			cout <<" " << node[j] ; 
 		cout << " \t" ;
@@ -145,6 +160,110 @@ void insertBack(Bucket *b,int value, int overflow_option)
 
 }
 
+// Splitting bucket only
+void bucketSplit(HashTable &h,int value,int orig_loc)
+{
+	//cout<<"Location:"<<orig_loc<<endl;
+	Bucket* b=h.directory[orig_loc];
+	Bucket* nb=new Bucket(h.bucket_size,h.directory[orig_loc]->local_depth);
+	vector<Bucket*> directory=h.directory;
+	vector<int> node= directory[orig_loc]->node;
+	//vector<int> overflow_node = directory[orig_loc]->overflow_page;
+	emptyBucket(directory[orig_loc]); // Delete the all the entry from the existing Original Bucket ID 
+	// Assign Empty node to image Bucket
+	int dir_size = h.directory.size();
+	int dir_half_size = dir_size/2;
+	int image_loc;
+	if (orig_loc < dir_half_size)
+	{	image_loc = orig_loc + dir_half_size;
+	}
+	else
+	{	image_loc = orig_loc -dir_half_size ;
+	}
+	directory[image_loc] = nb;	
+	// Reassigning and re-inserting each entry of current loc  
+	for(int i=0;i<node.size();i++)
+	{
+		int loc=hashCode(node[i],directory.size());
+		insertBack(directory[loc],node[i], h.overflow_flag);
+		//directory[loc]->cur_size +=1;
+
+	}
+
+	// Pushing the new element value
+	int loc=hashCode(value,directory.size());
+	insertBack(directory[loc],value, h.overflow_flag);
+	//directory[loc]->cur_size +=1;
+	// Increase local depth of Original bucket and Image Bucket ID 
+	directory[orig_loc]->local_depth++;
+	directory[image_loc]->local_depth++;
+	cout<<"INFO : inserted at"<< loc << endl;
+
+	h.directory=directory;
+
+}
+
+
+// Doubling Directory and spliting bucket
+void insertSplit(HashTable &h,int value)
+{
+	vector<Bucket*> directory=h.directory;
+	int orig_loc=hashCode(value,directory.size());
+	 
+	int dir_size=directory.size();
+	int dir_half_size = dir_size/2;
+	for(int i=0;i<dir_size;i++)
+	{	Bucket* b= new Bucket(h.bucket_size,h.directory[i]->local_depth);;
+		
+		directory.push_back(b);	
+		if(i!=orig_loc)
+			directory[i+dir_size] = directory[i];  // new Bucket(h.bucket_size,h.directory[orig_loc]->local_depth);
+		
+	}
+	cout<<"INFO : Directory doubled and Bucket Splitted; dir_size :  " << directory.size() << endl;
+	h.global_depth=log2(directory.size());
+	// Re-distributing the keys across the splitted bucket for cur loc
+	vector<int> node=directory[orig_loc]->node;
+	vector<int> overflow_node = directory[orig_loc]->overflow_page;
+	emptyBucket(directory[orig_loc]); // Delete the all the entry from the existing Original Bucket ID 
+	//emptyBucket(directory[orig_loc]->overflow_page);
+	//Redistributing  and re-inserting elements of Orginal Bucket between  Image bucket and Original bucket
+	for(int i=0;i<node.size();i++)
+	{
+		int loc =hashCode(node[i],directory.size());
+		//cout << " Node element processed for Reditribution : " << node[i] <<  " at loc : " << loc <<endl;
+		insertBack(directory[loc],node[i], h.overflow_flag);
+	}
+	if(h.overflow_flag ==1)
+	{
+		for(int i=0;i<overflow_node.size();i++)
+		{
+			int loc =hashCode(overflow_node[i],directory.size());
+			//cout << " Node element processed : " << node[i] << endl;
+			insertBack(directory[loc],overflow_node[i],h.overflow_flag);
+			//directory[loc]->cur_size +=1;
+		}
+	}
+	//Bucket *b;
+	//vector<int> *overflownode=b->overflow_page;
+	//overflow_node->erase(overflow_node->begin(),overflow_node->end());
+	// Pushing the new element value
+	int loc=hashCode(value,directory.size());
+	insertBack(directory[loc],value, h.overflow_flag);
+	//directory[loc]->cur_size +=1;
+	// Increase local depth of Original bucket and Image Bucket ID 
+	int image_loc = orig_loc + directory.size()/2;
+	directory[orig_loc]->local_depth++;
+	directory[image_loc]->local_depth++;
+	cout<<"INFO Line 209: inserted at "<< loc << endl;
+	cout << " DEBUG INFO Line 210 : " <<  " Original loc : " << orig_loc << " Image loc : " << image_loc << endl;
+	cout<<" DEBUG INFO Line 211 : local depth of "<<  orig_loc << directory[orig_loc]->local_depth++<< endl;
+cout<<" DEBUG INFO Line 212: local depth of "<<  orig_loc << directory[image_loc]->local_depth++<< endl;
+	h.directory=directory; // Re-assigning the Directory
+	h.global_depth++;
+	//cout<<"readjusting"<<endl;
+	//reAdjustPointers(h, orig_loc);
+}
 
 void insert(int value,HashTable &h)
 {
@@ -156,8 +275,13 @@ void insert(int value,HashTable &h)
 	//int loc_node = searchNode(value,h.directory[loc]->node);
 	
 	if(isFull(directory[loc],h.overflow_flag))
-	{
-		return;
+	{	cout<<"INFO : Bucket ID :  " << loc << " is Full "<<endl;
+		int local_depth=directory[loc]->local_depth,global_depth=h.global_depth;
+		if(local_depth==global_depth) //Splitting bucket & Double Directory
+		{
+			cout << "INFO : Splitting the Bucket and Doubling the Directory "<<endl;
+			insertSplit(h,value);
+		}
 	}
 	else
 	{
@@ -214,13 +338,13 @@ int main ( int argc, char *argv[] )
 				command = line1;
 			else if(count==2)
 			{
-				value = stoi(line1) ;
+				value = atoi(line1.c_str()) ;
 				//cout << " value ::"<< value<<";"<<endl;;
 			}
 			else
 				break;
 		}
-		//cout << "Command : "  << command << ",value :  " << value << endl;
+		cout << "Command : "  << command << ",value :  " << value << endl;
 		//cout << endl;
 		ss.clear();
 
